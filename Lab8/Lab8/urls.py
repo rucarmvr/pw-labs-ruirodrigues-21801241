402c63b0b269ecf24f55ea224d06e69387a387b4
rucarmvr <a21801241@alunos.ulht.pt>
2022-04-27 11:34:20 +0100

    pwlab8

--- Lab8/Lab8/urls.py
+++ Lab8/Lab8/urls.py
@@ -1,4 +1,4 @@
-"""Lab8 URL Configuration
+"""lab8 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
