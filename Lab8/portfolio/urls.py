9b64723b3bb48f89551b13508661f2b164125d38
rucarmvr <a21801241@alunos.ulht.pt>
2022-06-01 11:19:43 +0100

    kj

---
+++ Lab8/portfolio/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+app_name = 'portfolio'
+
+urlpatterns = [
+    path('', views.home_page_view, name='home'),
+    path('home', views.home_page_view, name='home'),
+    path('home_web', views.home_web_page_view, name='home_web'),
+    path('about', views.about_page_view, name='about'),
+    path('projects', views.projects_page_view, name='projects'),
+    path('web', views.web_page_view, name='web'),
+    path('contact', views.contact_page_view, name='contact'),
+    path('licenciatura', views.licenciatura_page_view, name='licenciatura'),
+    path('blog', views.blog_page_view, name='blog'),
+    path('index', views.index_page_view, name='index'),
+    path('login', views.login_page_view, name='login'),
+    path('logout', views.logout_page_view, name='logout'),
+]
