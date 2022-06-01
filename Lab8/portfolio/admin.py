ff5bcc0f26f0423d71073002272a0282d157eb07
rucarmvr <a21801241@alunos.ulht.pt>
2022-06-01 18:21:24 +0100

    k

--- Lab8/portfolio/admin.py
+++ Lab8/portfolio/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Projetos, Home, Home_Web, Web
 
-# Register your models here.
+# register
+
+admin.site.register(Home)
+admin.site.register(Home_Web)
+admin.site.register(Projetos)
+admin.site.register(Web)
--- Lab8/portfolio/views.py
+++ Lab8/portfolio/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from portfolio.models import Projetos, Home, Home_Web, Web
 
-# Create your views here.
+
+def home_page_view(request):
+    context = {'home': Home.objects.all()}
+    return render(request, 'portfolio/home.html', context)
+
+
+def home_web_page_view(request):
+    context = {'home_web': Home_Web.objects.all()}
+    return render(request, 'portfolio/home.html', context)
+
+
+def web_page_view(request):
+    context = {'web': Web.objects.all()}
+    return render(request, 'portfolio/web.html', context)
+
+
+def about_page_view(request):
+    return render(request, 'portfolio/about.html')
+
+
+def projects_page_view(request):
+    context = {'projects': Projetos.objects.all()}
+    return render(request, 'portfolio/projects.html', context)
+
+
+def contact_page_view(request):
+    return render(request, 'portfolio/contact.html')
+
+
+def licenciatura_page_view(request):
+    return render(request, 'portfolio/licenciatura.html')
+
+
+def blog_page_view(request):
+    return render(request, 'portfolio/blog.html')
+
