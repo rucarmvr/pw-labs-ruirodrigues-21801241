d721197bc222f1a2fdf44c71020976e36cbc90d6
rucarmvr <a21801241@alunos.ulht.pt>
2022-06-02 21:31:50 +0100

    k

--- Lab8/portfolio/models.py
+++ Lab8/portfolio/models.py
@@ -17,21 +17,21 @@ class Web(models.Model):
 
 
 class Home(models.Model):
-    titulo = models.CharField(max_length=20)
-    subtitulo = models.CharField(max_length=20)
-    texto = models.CharField(max_length=200)
+    titulo = models.CharField(max_length=200)
+    subtitulo = models.CharField(max_length=200)
+    texto = models.CharField(max_length=2000)
     image = models.ImageField(upload_to='image', null=True)
 
 
 class Projetos(models.Model):
-    titulo = models.CharField(max_length=20)
-    subtitulo = models.CharField(max_length=20)
+    titulo = models.CharField(max_length=200)
+    subtitulo = models.CharField(max_length=200)
     texto = models.CharField(max_length=200)
     image = models.ImageField(upload_to='image', null=True)
 
 
 class Licenciatura(models.Model):
-    titulo = models.CharField(max_length=20)
-    subtitulo = models.CharField(max_length=20)
+    titulo = models.CharField(max_length=200)
+    subtitulo = models.CharField(max_length=200)
     texto = models.CharField(max_length=200000000)
     image = models.ImageField(upload_to='image', null=True)
