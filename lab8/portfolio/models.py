f61f36573560ce105176bf6de146afdf4df56d44
rucarmvr <a21801241@alunos.ulht.pt>
2022-06-05 16:00:52 +0100

    comiti do lab8 5junho

--- lab8/portfolio/models.py
+++ lab8/portfolio/models.py
@@ -17,15 +17,16 @@ class Web(models.Model):
 
 
 class Home(models.Model):
-    titulo = models.CharField(max_length=200)
-    subtitulo = models.CharField(max_length=200)
-    texto = models.CharField(max_length=2000)
+    titulo = models.CharField(max_length=20)
+    subtitulo = models.CharField(max_length=20)
+    texto = models.CharField(max_length=200)
+    url = models.CharField(max_length=250)
     image = models.ImageField(upload_to='image', null=True)
 
 
 class Projetos(models.Model):
-    titulo = models.CharField(max_length=200)
-    subtitulo = models.CharField(max_length=200)
+    titulo = models.CharField(max_length=20)
+    subtitulo = models.CharField(max_length=20)
     texto = models.CharField(max_length=200)
     image = models.ImageField(upload_to='image', null=True)
 
@@ -33,29 +34,31 @@ class Projetos(models.Model):
 class Licenciatura(models.Model):
     titulo = models.CharField(max_length=200)
     subtitulo = models.CharField(max_length=200)
-    texto = models.CharField(max_length=200000000)
-    image = models.ImageField(upload_to='image', null=True)
+    texto = models.TextField()
+    texto2 = models.TextField()
+    texto3 = models.TextField()
+    texto4 = models.TextField()
 
 
 class About(models.Model):
     logo = models.ImageField(upload_to='image', null=True)
-    titulo = models.CharField(max_length=200)
-    texto = models.CharField(max_length=20000)
+    titulo = models.CharField(max_length=20)
+    texto = models.CharField(max_length=200)
 
 
 class Educacao(models.Model):
-    titulo = models.CharField(max_length=200)
-    subtitulo = models.CharField(max_length=200)
-    texto = models.CharField(max_length=200000000)
+    titulo = models.CharField(max_length=20)
+    subtitulo = models.CharField(max_length=20)
+    texto = models.CharField(max_length=200)
 
 
 class Certificados(models.Model):
-    titulo = models.CharField(max_length=200)
-    subtitulo = models.CharField(max_length=200)
-    texto = models.CharField(max_length=200000000)
+    titulo = models.CharField(max_length=20)
+    subtitulo = models.CharField(max_length=20)
+    texto = models.CharField(max_length=200)
 
 
 class Interesses_hobbies(models.Model):
-    titulo = models.CharField(max_length=200)
-    subtitulo = models.CharField(max_length=200)
-    texto = models.CharField(max_length=200000000)
+    titulo = models.CharField(max_length=20)
+    subtitulo = models.CharField(max_length=20)
+    texto = models.CharField(max_length=200)
